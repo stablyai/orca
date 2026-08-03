ai-integr8tor/stablyai-orca

Support project-level Quick Commands shared via orca.yaml
Teams recreate the same terminal workflows per user because Quick
Commands only live in local settings. Add a `quickCommands` key to
orca.yaml so repos can commit shared commands: they load through the
existing hooks read paths (local/SSH/runtime), surface read-only in the
quick-commands menu, context menu, and settings with a copy-to-personal
action, and are trust-gated on first run like other repo-authored
commands. Closes #8481.

Generated with [Devin](https://devin.ai)

Co-Authored-By: Devin <158243242+devin-ai-integration[bot]@users.noreply.github.com>

src/renderer/src/store/index.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 import type { AppState } from './types'
 import { createRepoSlice } from './slices/repos'
 import { createSparsePresetsSlice } from './slices/sparse-presets'
+import { createProjectQuickCommandsSlice } from './slices/project-quick-commands'
 import { createWorktreeSlice } from './slices/worktrees'
 import { createTerminalSlice } from './slices/terminals'
 import { createTabsSlice } from './slices/tabs'
@@ -43,6 +44,7 @@ import { registerHttpLinkStoreAccessor } from '@/lib/http-link-routing'
 export const useAppStore = create<AppState>()((...a) => ({
   ...createRepoSlice(...a),
   ...createSparsePresetsSlice(...a),
+  ...createProjectQuickCommandsSlice(...a),
   ...createWorktreeSlice(...a),
   ...createTerminalSlice(...a),
   ...createTabsSlice(...a),

stablyai/orca

src/renderer/src/store/index.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 import type { AppState } from './types'
 import { createRepoSlice } from './slices/repos'
 import { createSparsePresetsSlice } from './slices/sparse-presets'
+import { createProjectQuickCommandsSlice } from './slices/project-quick-commands'
 import { createWorktreeSlice } from './slices/worktrees'
 import { createTerminalSlice } from './slices/terminals'
 import { createTabsSlice } from './slices/tabs'
@@ -95,7 +96,8 @@ export const useAppStore = create<AppState>()((...a) => {
     ...createOrcaProfilesSlice(...a),
     ...createNewIssueDraftSlice(...a),
     ...createTaskCreationDraftsSlice(...a),
-    ...createRemoteServerUpdatesSlice(...a)
+    ...createRemoteServerUpdatesSlice(...a),
+    ...createProjectQuickCommandsSlice(...a)
   }
 })
 